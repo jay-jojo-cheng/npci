8d426588069418afa09b5cb690e0185bc2f80438
J. Jojo Cheng <jay.jojo.cheng@gmail.com>
2022-07-01 12:17:09 -0700

    sync ubuntu

--- examples/ihdp_sim/runLocally.R
+++ examples/ihdp_sim/runLocally.R
@@ -23,28 +23,54 @@ for (i in seq_along(methods)) {
     for (k in seq_along(covariates)) {
       for (l in seq_along(settings)) {
         for (m in seq_along(p.score)) {
-          existingResults <- getResultIntervals(methods[i], overlap[j], covariates[k], settings[l], p.score[m])
+          existingResults <-
+            getResultIntervals(methods[i], overlap[j], covariates[k], settings[l], p.score[m])
           
-          resultsIntervals <- intervalSubtraction(baseResultInterval, existingResults)
-          if (nrow(resultsIntervals) == 0) next
+          resultsIntervals <-
+            intervalSubtraction(baseResultInterval, existingResults)
+          if (nrow(resultsIntervals) == 0)
+            next
           
           Sys.setenv(METHOD = methods[i])
-          Sys.setenv(OVERLAP = if (overlap[j]) "true" else "false")
+          Sys.setenv(OVERLAP = if (overlap[j])
+            "true"
+            else
+              "false")
           Sys.setenv(COVARIATES = covariates[k])
           Sys.setenv(SETTING = settings[l])
           Sys.setenv(PROPENSITY = p.score[m])
           Sys.setenv(VERBOSE = verbose)
           
           for (n in seq_len(nrow(resultsIntervals))) {
-            interval <- resultsIntervals[n,,drop = FALSE]
+            interval <- resultsIntervals[n, , drop = FALSE]
             #if (interval[,"start"] == 134L && covariates[k] == "reduced" && settings[l] == "B" && p.score[m] == "bayesglm") next
-            Sys.setenv(START = interval[,"start"])
-            Sys.setenv(END   = interval[,"end"])
+            Sys.setenv(START = interval[, "start"])
+            Sys.setenv(END   = interval[, "end"])
             
-            cat("running method '", methods[i], "' with overlap = ", overlap[j], ", covariates = '", covariates[k],
-                "', setting = ", settings[l], ", prop = ", p.score[m], " from ", interval[,"start"], " to ", interval[,"end"], "\n", sep = "")
+            cat(
+              "running method '",
+              methods[i],
+              "' with overlap = ",
+              overlap[j],
+              ", covariates = '",
+              covariates[k],
+              "', setting = ",
+              settings[l],
+              ", prop = ",
+              p.score[m],
+              " from ",
+              interval[, "start"],
+              " to ",
+              interval[, "end"],
+              "\n",
+              sep = ""
+            )
             
-            source("runJob.R", local = new.env(parent = .GlobalEnv), keep.source = FALSE)
+            source(
+              "runJob.R",
+              local = new.env(parent = .GlobalEnv),
+              keep.source = FALSE
+            )
           }
         }
       }
--- examples/toy/generatePlots.R
+++ examples/toy/generatePlots.R
@@ -1,10 +1,15 @@
 require(npci)
 
 generateToyData <- function() {
-  f0 <- function(x) 72 + 3 * sqrt(x)
-  f1 <- function(x) 90 + exp(0.06 * x)
+  f0 <- function(x)
+    72 + 3 * sqrt(x)
+  f1 <- function(x)
+    90 + exp(0.06 * x)
   
-  oldSeed <- if (exists(".Random.seed")) .Random.seed else NULL
+  oldSeed <- if (exists(".Random.seed"))
+    .Random.seed
+  else
+    NULL
   set.seed(997L)
   
   ## generate true values
@@ -12,7 +17,8 @@ generateToyData <- function() {
   p <- 0.5
   
   z <- rbinom(n, 1L, p)
-  n1 <- sum(z); n0 <- n - n1
+  n1 <- sum(z)
+  n0 <- n - n1
   
   x <- numeric(n)
   x[z == 0] <- rnorm(n0, 20, 10)
@@ -22,7 +28,8 @@ generateToyData <- function() {
   y[z == 1] <- f1(x[z == 1])
   y <- y + rnorm(n)
   
-  if (!is.null(oldSeed)) .Random.seed <- oldSeed
+  if (!is.null(oldSeed))
+    .Random.seed <- oldSeed
   
   npci:::namedList(x, y, n, p, z, f0, f1)
 }
@@ -34,74 +41,107 @@ getPlotXValues <- function(x, n.points = 101L)
   seq(xRange[1], xRange[2], length.out = n.points)
 }
 
-getPlotYValues <- function(y, x, z, f0, f1, xValues, method, estimand = NULL, prob.z = NULL)
-{
-  if (method == "truth")
-    return(list(y.0 = f0(xValues), y.1 = f1(xValues)))
-  
-  fit <- ci.fit(y, x, z, method, estimand, prob.z)
-  
-  if (method %in% c("naive1", "naive2")) {
-    pred.0 <- predict(fit, xValues, 0, "se")
-    pred.1 <- predict(fit, xValues, 1, "se")
+getPlotYValues <-
+  function(y,
+           x,
+           z,
+           f0,
+           f1,
+           xValues,
+           method,
+           estimand = NULL,
+           prob.z = NULL)
+  {
+    if (method == "truth")
+      return(list(y.0 = f0(xValues), y.1 = f1(xValues)))
     
-    y.hat.0 <- pred.0$fit
-    y.hat.1 <- pred.1$fit
+    fit <- ci.fit(y, x, z, method, estimand, prob.z)
     
-    se.0    <- pred.0$se
-    se.1    <- pred.1$se
+    if (method %in% c("naive1", "naive2")) {
+      pred.0 <- predict(fit, xValues, 0, "se")
+      pred.1 <- predict(fit, xValues, 1, "se")
+      
+      y.hat.0 <- pred.0$fit
+      y.hat.1 <- pred.1$fit
+      
+      se.0    <- pred.0$se
+      se.1    <- pred.1$se
+      
+      poly.y.0 <-
+        cbind(y.hat.0 - se.0 * qnorm(0.975), rev(y.hat.0 + se.0 * qnorm(0.975)))
+      poly.y.1 <-
+        cbind(y.hat.1 - se.1 * qnorm(0.975), rev(y.hat.1 + se.1 * qnorm(0.975)))
+    } else if (method == "bart") {
+      pred.0 <- predict(fit, xValues, 0, keeptrainfits = FALSE)
+      pred.1 <- predict(fit, xValues, 1, keeptrainfits = FALSE)
+      
+      y.hat.0 <- pred.0$mean
+      y.hat.1 <- pred.1$mean
+      
+      lower.0 <- pred.0$bound[1, ]
+      lower.1 <- pred.1$bound[1, ]
+      upper.0 <- pred.0$bound[2, ]
+      upper.1 <- pred.1$bound[2, ]
+      
+      poly.y.0 <- cbind(upper.0, rev(lower.0))
+      poly.y.1 <- cbind(upper.1, rev(lower.1))
+    } else if (method %in%  c("grouped", "independent")) {
+      optPars <-
+        if (method == "grouped")
+          fit@env$opt[[1]]$par
+      else
+        rbind(fit@env$opt.0[[1]]$par, fit@env$opt.1[[1]]$par)
+      pred.0 <- predict(fit, xValues, 0, "se", pars = optPars)
+      pred.1 <- predict(fit, xValues, 1, "se", pars = optPars)
+      
+      y.hat.0 <- pred.0$fit
+      y.hat.1 <- pred.1$fit
+      
+      se.0    <- pred.0$se
+      se.1    <- pred.1$se
+      
+      poly.y.0 <-
+        cbind(y.hat.0 - se.0 * qnorm(0.975), rev(y.hat.0 + se.0 * qnorm(0.975)))
+      poly.y.1 <-
+        cbind(y.hat.1 - se.1 * qnorm(0.975), rev(y.hat.1 + se.1 * qnorm(0.975)))
+    } else if (method == "btgp") {
+      pred.0 <- predict(fit, xValues, 0, pred.n = FALSE)
+      pred.1 <- predict(fit, xValues, 1, pred.n = FALSE)
+      
+      y.hat.0 <- pred.0$fit
+      y.hat.1 <- pred.1$fit
+      
+      se.0    <- sqrt(diag(pred.0$vcov))
+      se.1    <- sqrt(diag(pred.1$vcov))
+      
+      poly.y.0 <-
+        cbind(y.hat.0 - se.0 * qnorm(0.975), rev(y.hat.0 + se.0 * qnorm(0.975)))
+      poly.y.1 <-
+        cbind(y.hat.1 - se.1 * qnorm(0.975), rev(y.hat.1 + se.1 * qnorm(0.975)))
+    }
     
-    poly.y.0 <- cbind(y.hat.0 - se.0 * qnorm(0.975), rev(y.hat.0 + se.0 * qnorm(0.975)))
-    poly.y.1 <- cbind(y.hat.1 - se.1 * qnorm(0.975), rev(y.hat.1 + se.1 * qnorm(0.975)))
-  } else if (method == "bart") {
-    pred.0 <- predict(fit, xValues, 0, keeptrainfits = FALSE)
-    pred.1 <- predict(fit, xValues, 1, keeptrainfits = FALSE) 
-    
-    y.hat.0 <- pred.0$mean
-    y.hat.1 <- pred.1$mean
-    
-    lower.0 <- pred.0$bound[1,]
-    lower.1 <- pred.1$bound[1,]
-    upper.0 <- pred.0$bound[2,]
-    upper.1 <- pred.1$bound[2,]
-    
-    poly.y.0 <- cbind(upper.0, rev(lower.0))
-    poly.y.1 <- cbind(upper.1, rev(lower.1))
-  } else if (method %in%  c("grouped", "independent")) {
-    optPars <- if (method == "grouped") fit@env$opt[[1]]$par else rbind(fit@env$opt.0[[1]]$par, fit@env$opt.1[[1]]$par)
-    pred.0 <- predict(fit, xValues, 0, "se", pars = optPars)
-    pred.1 <- predict(fit, xValues, 1, "se", pars = optPars)
-    
-    y.hat.0 <- pred.0$fit
-    y.hat.1 <- pred.1$fit
-    
-    se.0    <- pred.0$se
-    se.1    <- pred.1$se
-    
-    poly.y.0 <- cbind(y.hat.0 - se.0 * qnorm(0.975), rev(y.hat.0 + se.0 * qnorm(0.975)))
-    poly.y.1 <- cbind(y.hat.1 - se.1 * qnorm(0.975), rev(y.hat.1 + se.1 * qnorm(0.975)))
-  } else if (method == "btgp") {
-    pred.0 <- predict(fit, xValues, 0, pred.n = FALSE)
-    pred.1 <- predict(fit, xValues, 1, pred.n = FALSE)
-    
-    y.hat.0 <- pred.0$fit
-    y.hat.1 <- pred.1$fit
-    
-    se.0    <- sqrt(diag(pred.0$vcov))
-    se.1    <- sqrt(diag(pred.1$vcov))
-    
-    poly.y.0 <- cbind(y.hat.0 - se.0 * qnorm(0.975), rev(y.hat.0 + se.0 * qnorm(0.975)))
-    poly.y.1 <- cbind(y.hat.1 - se.1 * qnorm(0.975), rev(y.hat.1 + se.1 * qnorm(0.975)))
+    list(
+      y.0 = f0(xValues),
+      y.hat.0 = y.hat.0,
+      y.1 = f1(xValues),
+      y.hat.1 = y.hat.1,
+      poly.y.0 = poly.y.0,
+      poly.y.1 = poly.y.1
+    )
   }
-  
-  list(y.0 = f0(xValues), y.hat.0 = y.hat.0, y.1 = f1(xValues), y.hat.1 = y.hat.1, poly.y.0 = poly.y.0, poly.y.1 = poly.y.1)
-}
 
 plotFit <- function(y, x, z, yValues, xValues, yRange, main)
 {
   par(mar = c(1.5, 1.5, 1.4, 0.1))
-  plot(NULL, type = "n", xlim = range(xValues), ylim = yRange, xlab = "", ylab = "",
-       main = main)
+  plot(
+    NULL,
+    type = "n",
+    xlim = range(xValues),
+    ylim = yRange,
+    xlab = "",
+    ylab = "",
+    main = main
+  )
   
   if (is.null(yValues$y.hat.0)) {
     lines(xValues, yValues$y.0, col = "gray")
@@ -110,12 +150,18 @@ plotFit <- function(y, x, z, yValues, xValues, yRange, main)
   } else {
     poly.x <- cbind(xValues, rev(xValues))
     
-    polygon(poly.x, yValues$poly.y.0, col = rgb(0.95, 0.95, 0.95), border = NA)
-    polygon(poly.x, yValues$poly.y.1, col = rgb(0.9, 0.9, 0.9), border = NA)
+    polygon(poly.x,
+            yValues$poly.y.0,
+            col = rgb(0.95, 0.95, 0.95),
+            border = NA)
+    polygon(poly.x,
+            yValues$poly.y.1,
+            col = rgb(0.9, 0.9, 0.9),
+            border = NA)
     lines(xValues, yValues$y.hat.0, col = "gray")
     lines(xValues, yValues$y.hat.1)
     lines(xValues, yValues$y.0, lwd = 0.7, col = "red")
-    lines(xValues, yValues$y.1,, lwd = 0.7, col = "red")
+    lines(xValues, yValues$y.1, , lwd = 0.7, col = "red")
     points(x, y, col = ifelse(z == 1, "black", "gray"), pch = 20)
   }
 }
@@ -125,20 +171,29 @@ generatePlots <- function(path = ".") {
   attach(toyData)
   
   xValues <- getPlotXValues(x)
-
+  
   yValues.truth <- getPlotYValues(y, x, z, f0, f1, xValues, "truth")
   yValues.bart  <- getPlotYValues(y, x, z, f0, f1, xValues, "bart")
-  yValues.group <- getPlotYValues(y, x, z, f0, f1, xValues, "grouped")
-  yValues.indep <- getPlotYValues(y, x, z, f0, f1, xValues, "independent")
+  yValues.group <-
+    getPlotYValues(y, x, z, f0, f1, xValues, "grouped")
+  yValues.indep <-
+    getPlotYValues(y, x, z, f0, f1, xValues, "independent")
   yValues.btgp  <- getPlotYValues(y, x, z, f0, f1, xValues, "btgp")
-
-  yRange <- range(yValues.truth$y.0, yValues.truth$y.1,
-                  yValues.bart$y.hat.0, yValues.bart$y.hat.1,
-                  yValues.group$y.hat.0, yValues.group$y.hat.1,
-                  yValues.indep$y.hat.0, yValues.indep$y.hat.1,
-                  yValues.btgp$y.hat.0, yValues.btgp$y.hat.1)
+  
+  yRange <- range(
+    yValues.truth$y.0,
+    yValues.truth$y.1,
+    yValues.bart$y.hat.0,
+    yValues.bart$y.hat.1,
+    yValues.group$y.hat.0,
+    yValues.group$y.hat.1,
+    yValues.indep$y.hat.0,
+    yValues.indep$y.hat.1,
+    yValues.btgp$y.hat.0,
+    yValues.btgp$y.hat.1
+  )
   yRange <- 1.02 * (yRange - mean(yRange)) + mean(yRange)
-
+  
   
   pdf(file.path(path, "toy_truth.pdf"), 3.5, 3.5)
   plotFit(y, x, z, yValues.truth, xValues, yRange, "Truth")
